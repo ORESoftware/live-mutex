cfaeb39c4ffa1e668dbcb0d22638daf67b24e07e
Olegzandr VD <alex@oresoftware.com>
2017-10-19 22:51:59 -0700

    pdev:set

--- @test.sh
+++ @test.sh
@@ -33,7 +33,7 @@ WHICH_SUMAN=$(which suman);
 if [[ -z ${WHICH_SUMAN} || "${NODE_ENV}" != "local" ]]; then
     echo "installing suman locally...";
 #    npm install -g sumanjs@latest --silent
-     npm install -g "github:sumanjs/suman#feature_alexamil_1508459456817" 2> /dev/null
+     npm install -g "sumanjs/suman#feature_alexamil_1508459456817" 2> /dev/null
 fi
 
 suman --coverage test/@src/*.ts #--inherit-stdio # --inherit-all-stdio
