85a8b1af8439ccaf652fbb0fd6107a6fa6816615
Olegzandr VD <alex@oresoftware.com>
2018-05-19 12:44:49 -0700

    pdev:set

--- broker.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var broker_1 = require("./lib/broker");
-exports.Broker = broker_1.Broker;
--- client.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var client_1 = require("./lib/client");
-exports.Client = client_1.Client;
