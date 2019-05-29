acd13553f2a6017bca83c2108d093bc345aaf1a3
Alexander Mills <alex@alexs-mac.local>
2019-05-28 17:47:56 -0700

    tmp

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -3,9 +3,15 @@
 
 set -e;
 
+cd `dirname $(dirname "$BASH_SOURCE")`
+
 version=`read_json -k 'version' -f package.json`;
 semver "$version"; # validate semver version
+
+tsc
 
 docker build -t "oresoftware/live-mutex-broker:$version" .
 
 docker push "oresoftware/live-mutex-broker:$version"
+
+
