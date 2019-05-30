4782d5399a4d1cc9c8d010df939a76a5108b11d5
Alexander Mills <alex@alexs-mac.local>
2019-05-30 12:56:24 -0700

    tmp

--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 
 
 <a align="right" href="https://travis-ci.org/ORESoftware/live-mutex">
-    <img align="right" alt="Travis Build Status" src="https://travis-ci.org/ORESoftware/live-mutex.svg?branch=master">
+    <img align="right" alt="Travis Build Status" src="https://travis-ci.org/ORESoftware/live-mutex.svg?branch=dev">
 </a>
 
 <br>
 
 <a align="right" href="https://circleci.com/gh/ORESoftware/live-mutex">
-    <img align="right" alt="CircleCI Build Status" src="https://circleci.com/gh/ORESoftware/live-mutex.png?circle-token=8ee83a1b06811c9a167e71d12b52f8cf7f786581">
+    <img align="right" alt="CircleCI Build Status" src="https://circleci.com/gh/ORESoftware/live-mutex.png?branch=dev&circle-token=8ee83a1b06811c9a167e71d12b52f8cf7f786581">
 </a>
 
 <br>
--- scripts/circleci/run.sh
+++ scripts/circleci/run.sh
@@ -1,38 +1,9 @@
 #!/usr/bin/env bash
 
+set -e;
 
-if [[ ! -d "node_modules" ]]; then
-    echo "error: node_modules directory is not present...run npm install as needed.";
-    exit 1;
-fi
-
-npm config set unsafe-perm true
-
-lib_name="live-mutex";
-
-export PATH=${PATH}:"$(pwd)/node_modules/.bin"
-
-if ! which suman-tools &> /dev/null ; then
-    npm install -g suman-tools;
-fi
-
-IS_GLOBALLY_SYMLINKED=`suman-tools --is-symlinked-globally="${lib_name}"`
-IS_LOCALLY_SYMLINKED=`suman-tools --is-symlinked-locally="${lib_name}"`
-
-if [[ ${IS_GLOBALLY_SYMLINKED} != *"affirmative"* ]]; then
-     npm link # create a global symlink for this library, so that we can create a local symlink
-fi
-
-if [[ ${IS_LOCALLY_SYMLINKED} != *"affirmative"* || ${IS_GLOBALLY_SYMLINKED} != *"affirmative"* ]]; then
-     npm link "${lib_name}" # create a global symlink for this library, so that we can create a local symlink
-fi
-
-
-if ! which suman &> /dev/null ; then
-     npm install -g suman@1.1.51244;
-fi
-
-
-npm link  suman
-
-suman -- #coverage test/@src/*.ts # --inherit-all-stdio #--inherit-stdio
+npm i
+tsc
+npm link suman
+ln -s `pwd` node_modules/live-mutex
+suman --default
