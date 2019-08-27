dba53e78057f1ad8bcee463123a13807ea42a1db
Alexander Mills <alex@alexs-mac.local>
2019-08-26 18:26:39 -0700

    tmp

--- test/setup-test.sh
+++ test/setup-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail;
+set -eo pipefail
 cd "$(dirname "$(dirname "$BASH_SOURCE")")"
 
 rm -rf node_modules
@@ -9,9 +9,11 @@ npm link -f
 npm link live-mutex
 npm link suman
 
-npm i --no-save 'async@2.6.3'
-npm i --no-save 'handlebars@4.1.1'
+#npm i --no-save 'async@2.6.3'
+#npm i --no-save 'handlebars@4.1.1'
 
+npm link --no-save 'async@2.6.3'
+npm link --no-save 'handlebars@4.1.1'
 
-# "node-redis-warlock": "^0.2.0",
-# "redis": "^2.6.3"
+npm link 'node-redis-warlock@^0.2.0'
+npm link 'redis@^2.6.3'
