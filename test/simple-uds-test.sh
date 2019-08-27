1461aed8aeb1ce0ac490bd8680cdb708f497aa44
Alexander Mills <alex@alexs-mac.local>
2019-08-27 00:17:07 -0700

    tmp

--- test/simple-uds-test.sh
+++ test/simple-uds-test.sh
@@ -6,17 +6,12 @@ cd  "$(dirname "${BASH_SOURCE[0]}")";
 
 echo "pwd: $(pwd)"
 
-#my_sock="$(pwd)/fixtures/uds.sock";
+my_sock="$(pwd)/fixtures/uds.sock";
 
-my_sock="$HOME/.lmx/uds.sock"
-
-mkdir -p "$HOME/.lmx";
-
+chmod -R 777 "$(pwd)/fixtures"
 rm -f "$my_sock"
 
-#docker run -d -v "$(pwd)/fixtures":/uds 'oresoftware/live-mutex-broker:4' --use-uds
-
-node
+docker run -d -v "$(pwd)/fixtures":/uds 'oresoftware/live-mutex-broker:4' --use-uds
 
 sleep 4;
 
