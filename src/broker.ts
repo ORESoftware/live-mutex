9736bd0e7ccbc5d26cc4f2cb729e872d132401cb
Alexander Mills <alex@alexs-mac.local>
2019-08-29 17:23:17 -0700

    tmp

--- src/broker.ts
+++ src/broker.ts
@@ -31,7 +31,6 @@ export const log = {
   }
 };
 
-///////////////////////////////////////////////////////////////////
 
 import {weAreDebugging} from './we-are-debugging';
 import {EventEmitter} from 'events';
