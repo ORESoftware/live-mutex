74d6927cca43bf9d76ac372366a504afd5fdc84a
Alexander Mills <alexander.d.mills@gmail.com>
2026-06-02 19:39:30 -0500

    expand r2g phase coverage

--- .r2g/config.js
+++ .r2g/config.js
@@ -1,23 +1,28 @@
-'use strict';
-
 const path = require('path');
 
-if (!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')) {
-  throw new Error('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute path.');
+const workspaceRoot = path.resolve(
+  process.env.R2G_SEARCH_ROOT ||
+  process.env.MY_DOCKER_R2G_SEARCH_ROOT ||
+  path.resolve(__dirname, '../..')
+);
+
+if (!path.isAbsolute(workspaceRoot)) {
+  throw new Error('Please set R2G_SEARCH_ROOT or MY_DOCKER_R2G_SEARCH_ROOT to an absolute path.');
 }
 
+const projectRoot = path.resolve(__dirname, '..');
+
 exports.default = {
-
-  searchRoot: path.resolve(process.env.MY_DOCKER_R2G_SEARCH_ROOT),
-
-  // the following packages will be installed in the Docker container using this pattern:
-  // npm install /r2g_shared_dir/Users/you/
-
+  searchRoot: projectRoot,
+  searchRoots: [
+    path.resolve(workspaceRoot, 'json-stream-parser'),
+    path.resolve(workspaceRoot, 'linked-queue'),
+    path.resolve(workspaceRoot, 'suman')
+  ],
   tests: '',
-
   packages: {
+    '@oresoftware/json-stream-parser': true,
     '@oresoftware/linked-queue': true,
     'suman': true
   }
-
 };
